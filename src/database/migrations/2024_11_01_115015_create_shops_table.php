b27faed82f2a479ba6ca5f114b64d1c16cbd4358
Katsumata <naoyakatsumata0708@gmail.com>
2024-11-01 20:35:42 +0900

    migrationファイル設定完了

---
+++ src/database/migrations/2024_11_01_115015_create_shops_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('shop_name');
+            $table->string('detail');
+            $table->unsignedBigInteger('category_id');
+            $table->string('img_path');
+            $table->unsignedBigInteger('area_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+};
